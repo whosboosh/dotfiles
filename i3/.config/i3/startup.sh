087adbad6b5baa3faa8f9ca46f3c16a41f83ec95
Nathanial Howard <nathanial55@gmail.com>
2021-01-25 03:18:21 +0000

    Conky

--- i3/.config/i3/startup.sh
+++ i3/.config/i3/startup.sh
@@ -5,3 +5,4 @@ picom -b &
 xinput --set-prop "MSHW0092:00 045E:0933 Touchpad" "libinput Natural Scrolling Enabled" 1 &
 ~/.config/polybar/launch.sh &
 redshift -P -O 3000 &
+xrandr --output eDP1 --auto --output HDMI1 --auto --left-of eDP1 &
