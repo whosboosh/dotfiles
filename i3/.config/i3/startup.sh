fe3a1aeb73435cf438f2a408c64bdd9d203b8e3b
Nathanial Howard <nathanial55@gmail.com>
2021-01-30 00:39:06 +0000

    Polybar update and rofi

--- i3/.config/i3/startup.sh
+++ i3/.config/i3/startup.sh
@@ -2,9 +2,9 @@
 
 xrandr --output eDP1 --auto --output HDMI1 --auto --left-of eDP1
 xinput --set-prop "MSHW0092:00 045E:0933 Touchpad" "libinput Natural Scrolling Enabled" 1
-picom --experimental-backends -b
+picom --experimental-backends
 redshift -P -O 3000
 nitrogen --restore
 light-locker
-~/.config/polybar/launch.sh
+~/.config/polybar/launch.sh &
 rclone --vfs-cache-mode writes mount onedrive: ~/onedrive
