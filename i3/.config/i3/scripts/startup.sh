5f10e2f1cbb07a3731e6af762b2ebd3cd71d8bae
Nathanial Howard <nathanial55@gmail.com>
2021-02-16 21:13:46 +0000

    autorandr stuff + kitty config

--- i3/.config/i3/scripts/startup.sh
+++ i3/.config/i3/scripts/startup.sh
@@ -1,12 +1,8 @@
-:x#!/bin/bash
-
-xrandr --output eDP1 --auto --output DP2-2 --auto --left-of eDP1 --output DP2-1 --auto --right-of eDP1
+#!/bin/bash
 xinput --set-prop "MSHW0092:00 045E:0933 Touchpad" "libinput Natural Scrolling Enabled" 1
-picom --experimental-backends &
-redshift -P -O 3000
+picom --experimental-backends
+redshift -P -O 3500
 nitrogen --restore
 light-locker
-~/.config/polybar/forest/launch.sh &
 rclone --vfs-cache-mode writes mount onedrive: ~/onedrive
 systemctl restart --user spotifyd
-trayer --edge top --align right --SetDockType false --SetPartialStrut false --expand true --width 6 --transparent true --alpha 1 --tint 0x141A21 --height 35
