f7abc28f4c781b973459d45c004cd1004a78f26a
Nathanial Howard <nathanial55@gmail.com>
2021-03-03 17:44:13 +0000

    Spotify polybar stuff

---
+++ polybar/.config/polybar/forest/scripts/web_spotify_status.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Get spotify tab name info
+var=$(bt list | grep "https://open.spotify.com")
+var=$(echo ${var%"https"*} | awk '{first=$1; $1=""; print $0; }')
+
+# Exit if spotify is not found
+if [[ -z $var ]] ; then
+    echo ""
+    exit 0
+fi
+
+# Print
+strMaxLen=40
+checkPaused=$(echo $var | awk '{print $1}')
+if [[ $checkPaused = "Spotify" ]] ; then
+    echo " Spotify is paused"
+else
+    song=${var%·*}
+    artist=${var#*·}
+    str="$artist:$song"
+    strLen=$(echo -n "$str" | wc -c)
+    if (( $strLen > $strMaxLen )) ; then
+        echo "${str:(0):($strMaxLen)}..."
+    else
+        echo "$str"
+    fi
+fi
