9dd078045d7085a67937b208764af86ad1988ee7
Nathanial Howard <nathanial55@gmail.com>
2024-05-20 08:40:21 +0100

    looking glass

--- i3/.config/i3/scripts/startup.sh
+++ i3/.config/i3/scripts/startup.sh
@@ -1,6 +1,7 @@
 xrandr --output HDMI-A-0 --mode 1920x1080 --rate 144.00
 xrandr --output DisplayPort-0 --mode 3840x2160 --rate 60.00 --left-of HDMI-A-0
 xrandr --output HDMI-0 --off
-feh --bg-fill ~/wallpapers/wp4779055-pink-anime-aesthetic-wallpapers.png
+#feh --bg-fill ~/wallpapers/wp4779055-pink-anime-aesthetic-wallpapers.png
+feh --bg-fill ~/wallpapers/VRChat_2024-03-24_05-03-16.132_3840x2160.png
 ~/.config/polybar/forest/launch.sh
 picom --animations --animation-stiffness-in-tag 300 --animation-clamping -b
